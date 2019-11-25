0b4dcb5771018df7944d556e7af57b835b6af5ca
Wim Jongman <wim.jongman@remainsoftware.com>
2019-11-25 13:18:38 +0100

    GIT-XG0029: Change the Original distribution file
    I removed redundant java doc comments

--- bundles/org.eclipse.ecf.examples.remoteservices.quotes.consumer/src/org/eclipse/ecf/examples/remoteservices/quotes/consumer/Activator.java
+++ bundles/org.eclipse.ecf.examples.remoteservices.quotes.consumer/src/org/eclipse/ecf/examples/remoteservices/quotes/consumer/Activator.java
@@ -11,13 +11,6 @@ public class Activator implements BundleActivator {
 		return context;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext
-	 * )
-	 */
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
 
